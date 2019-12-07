943da76ce3591ef23aa4fbf711a985c339861531
shivankitbhardwaj <50508669+shivankitbhardwaj@users.noreply.github.com>
2019-12-07 20:09:59 +0530

    Update Neural.s

--- Neural.s
+++ Neural.s
@@ -1,187 +1,239 @@
-; Neural network
-     AREA    neural,CODE,READONLY
+     AREA    exponential,CODE,READONLY
+     IMPORT printMsg
+	 IMPORT print1	 
      EXPORT __main
-	 IMPORT PrintMsg	 
-     ENTRY
-__main  FUNCTION
-; IGNORE THIS PART
-; select dataset logic
-	      MOV R8, #2 ; dataset selection
-		  MOV R1, #1 ; dataset 1
-		  MOV R2, #2 ; dataset 2
-		  MOV R3, #3 ; dataset 3
-		  MOV R4, #4 ; dataset 4
-		  
-		 CMP R8,R1 ; for dataset 1 
-		 BEQ dset1
-		 
-		 CMP R8,R2 ; for dataset 2
-		 BEQ dset2
-		 
-		 CMP R8,R3 ; for dataset 3
-		 BEQ dset3
-		 
-		 CMP R8,R4 ; for dataset 4
-		 BEQ dset4
-		 
+     ENTRY 
+__main  FUNCTION	
+; IGNORE THIS PART 	
+		;MOV R4,#1 ;for logic initial;AND LOGIC VALUE
+		MOV R12,#7 ;for logic increase
+		
+		MOV R5,#6 ;for logic or
+		MOV R6,#5 ;for logic not
+		MOV R7,#4 ;for logic nand 
+		MOV R8,#3;for logic nor 
+		MOV R9,#2 ;for logic xor
+		MOV R10,#1 ;for logic xnor 
 
-; dataset 1
-dset1	  VLDR.F32 s0 , =1 ;X0 DATA
-          VLDR.F32 s1 , =0 ;X1 DATA
-          VLDR.F32 s2 , =0 ;X2 DATA	
-		  VLDR.F32 s15 , =0 ;initialising sum of weights	
-		  b lsel
+		MOV R11,#7;for dataset selection		
+		MOV R3,#7;for dataset selection	
+		B logic_and
 
-; dataset 2
-dset2     VLDR.F32 s0 , =1 ;X0 DATA
-          VLDR.F32 s1 , =0 ;X1 DATA
-          VLDR.F32 s2 , =1 ;X2 DATA	
-		  VLDR.F32 s15 , =0 ;initialising sum of weights	
-		  b lsel
-		  
-; dataset 3
-dset3	  VLDR.F32 s0 , =1 ;X0 DATA
-          VLDR.F32 s1 , =1 ;X1 DATA
-          VLDR.F32 s2 , =0 ;X2 DATA	
-		  VLDR.F32 s15 , =0 ;initialising sum of weights	
-		  b lsel		  
-
-; dataset 4
-dset4	  VLDR.F32 s0 , =1 ;X0 DATA
-          VLDR.F32 s1 , =1 ;X1 DATA
-          VLDR.F32 s2 , =1 ;X2 DATA	
-		  VLDR.F32 s15 , =0 ;initialising sum of weights	
-		  b lsel
-		  
-;switch case logic
-
-lsel	  MOV R9, #4 ; logic selection
-		  MOV R1, #1 ; NAND_LOGIC
-		  MOV R2, #2 ; NOR_LOGIC
-		  MOV R3, #3 ; AND_LOGIC
-		  MOV R4, #4 ; OR_LOGIC
-		  MOV R5, #5 ; XOR_LOGIC
-		  MOV R6, #6 ; XNOR_LOGIC
-		  MOV R7, #7 ; NOT_LOGIC
-		  
-		 CMP R9,R1 ; NAND 
-		 BEQ NAND_LOGIC
-		 
-		 CMP R9,R2 ; NOR
-		 BEQ NOR_LOGIC
-		 
-		 CMP R9,R3 ; AND
-		 BEQ AND_LOGIC
-		 
-		 CMP R9,R4 ; OR
-		 BEQ OR_LOGIC
-		 
-		 CMP R9,R5 ; XOR
-		 BEQ XOR_LOGIC
-		 
-		 CMP R9,R6 ; XNOR 
-		 BEQ XNOR_LOGIC
-		 
-		 CMP R9,R7 ; NOT
-		 BEQ NOT_LOGIC
-		 
-		 
-NAND_LOGIC         VLDR.F32 s28 ,=0.6 	;WEIGHT W1   
-		           VLDR.F32 s29 ,=-0.8  ;WEIGHT W2   
-                   VLDR.F32	s30 ,=-0.8 	;WEIGHT W3
-                   VLDR.F32 s31 ,=0.3   ;BIAS   
-                   B  X_CALCULATION
+			 
+;LOOP_A  	    CMP R4,R5
+;				BEQ logic_or
+		
 
 
-NOR_LOGIC          VLDR.F32 s28 ,=0.5 	;WEIGHT W1   
-		           VLDR.F32 s29 ,=-0.7  ;WEIGHT W2   
-                   VLDR.F32	s30 ,=-0.7 	;WEIGHT W3
-                   VLDR.F32 s31 ,=0.1   ;BIAS   
-                   B  X_CALCULATION
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;logics to select;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-AND_LOGIC         VLDR.F32 s28 ,=-0.1 	;WEIGHT W1   
-		          VLDR.F32 s29 ,=0.2  ;WEIGHT W2   
-                  VLDR.F32	s30 ,=0.2 	;WEIGHT W3
-                  VLDR.F32 s31 ,=-0.2   ;BIAS   
-                   B  X_CALCULATION
+logic_and          ;weights w0 w1 w2
+				   VLDR.F32 s3 ,=-0.1 	;w0
+		           VLDR.F32 s4 ,=0.2    ;w1   
+                   VLDR.F32	s15 ,=0.2 	;w2
+				   ;Bias
+                   VLDR.F32 s6 ,=-0.2;  bias  
+                   B  dataset
 
-OR_LOGIC          VLDR.F32 s28 ,=-0.1	;WEIGHT W1   
-		          VLDR.F32 s29 ,=0.7  ;WEIGHT W2   
-                  VLDR.F32	s30 ,=0.7 	;WEIGHT W3
-                  VLDR.F32 s31 ,=-0.1  ;BIAS				  
-                   B  X_CALCULATION
 
-XOR_LOGIC         VLDR.F32 s28 ,=-5 	;WEIGHT W1   
-		          VLDR.F32 s29 ,=20  ;WEIGHT W2   
-                  VLDR.F32	s30 ,=10 	;WEIGHT W3
-                  VLDR.F32 s31 ,=1   ;BIAS   
-                   B  X_CALCULATION
+logic_or           ;weights w0 w1 w2
+				   VLDR.F32 s3 ,=-0.1 	;w0
+		           VLDR.F32 s4 ,=0.7    ;w1   
+                   VLDR.F32	s15 ,=0.7 	;w2
+				   ;Bias
+                   VLDR.F32 s6 ,=-0.1;  bias
+                  B  dataset
+			
+logic_not          ;weights w0 w1 w2
+				   VLDR.F32 s3 ,=0.5 	;w0
+		           VLDR.F32 s4 ,=-0.7   ;w1   
+		           VLDR.F32 s15 ,=0   ;w1   
+		   		   ;Bias
+                  VLDR.F32 s6 ,=0.1;   bias 
+                  B  dataset
 
-XNOR_LOGIC        VLDR.F32 s28 ,=-5 	;WEIGHT W1   
-		          VLDR.F32 s29 ,=20  ;WEIGHT W2   
-                  VLDR.F32	s30 ,=10 	;WEIGHT W3
-                  VLDR.F32 s31 ,=1   ;BIAS   
-                   B  X_CALCULATION
 
-NOT_LOGIC         VLDR.F32 s2 , =0
-                  VLDR.F32 s28 ,=0.5 	;WEIGHT W1   
-		          VLDR.F32 s29 ,=-0.7  ;WEIGHT W2   
-                  VLDR.F32	s30 ,=0 	;WEIGHT W3
-                  VLDR.F32 s31 ,=0.1   ;BIAS   
-                   B  X_CALCULATION
-		  
-X_CALCULATION		  VMUL.F32  s14 , s0 ,s28  ;S15 WILL HAVE SUM OF WEIGHTS
-                      VADD.F32  s15  , s15 ,s14
-		              VMUL.F32  s14 , s1 ,s29  
-					  VADD.F32  s15  , s15 ,s14
-					  VMUL.F32  s14 , s2 ,s30
-					  VADD.F32  s15  , s15 ,s14
-                      VADD.F32  s15 , s15,s31
-                      B SIGMOID					  
-					  
-SIGMOID	 VLDR.F32 S10,=1; x increment
-		 VMOV.F32 S2,S15 ; updating the x value
-		 BL SR
-		 VADD.F32 S11,S0,S10 ;  1+e^x
-		 VDIV.F32 S12,S0,S11 ;  e^x/(1+e^x)
-		 VMOV.F32 R2,S12 ; for display purpose
-		 VLDR.F32 S10,=0.5 ; sigmoid reference
-		 VMOV.F32 R4,S10
-		 MOV R0,R8 ; dataset selection display
-		 MOV R1,R9 ; logic selection display
-		 CMP R2,R4
-         MOVGE R3, #1
-		 MOVLT R3, #0
-		 BL PrintMsg
-stop     B stop;else goto stop		
+logic_nand         ;weights w0 w1 w2
+				   VLDR.F32 s3 ,=0.6 	;w0
+		           VLDR.F32 s4 ,=-0.8   ;w1   
+                   VLDR.F32	s15 ,=-0.8 	;w2
+				   ;Bias
+                   VLDR.F32 s6 ,=0.3;   bias   
+                   B  dataset
+				   
+				   
+logic_nor          ;weights w0 w1 w2
+				   VLDR.F32 s3 ,=0.5 	;w0
+		           VLDR.F32 s4 ,=-0.7   ;w1   
+                   VLDR.F32	s15 ,=-0.7 	;w2
+				   ;Bias
+                   VLDR.F32 s6 ,=0.1;   bias   
+                   B  calculation
+
+logic_xor          ;weights w0 w1 w2
+				   VLDR.F32 s3 ,=-5 	;w0
+		           VLDR.F32 s4 ,=20     ;w1   
+                   VLDR.F32	s15 ,=10 	;w2
+				   ;Bias
+                  VLDR.F32 s6 ,=1;   bias   
+                   B  dataset
+
+logic_xnor         ;weights w0 w1 w2
+				   VLDR.F32 s3 ,=-5 	;w0
+		           VLDR.F32 s4 ,=20     ;w1   
+                   VLDR.F32	s15 ,=10 	;w2
+				   ;Bias
+                   VLDR.F32 s6 ,=1;   bias   
+                   B  dataset
+
+;;;;;;;;;;;;;;;;;;;DATA SET SELECTION ;;;;;;;;;;;;;;;;;;;;;;;;
+
+dataset CMP R3,R11
+		BEQ set_1 
+
+       ; CMP R11,R5
+		;BEQ set_2 
+		
+        ;CMP R11,R6
+		;BEQ set_3 
+		
+        ;CMP R11,R7
+		;BEQ set_4 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;dataset selection;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+set_1   VLDR.F32 s0 , =1 ;data 1 (x1)
+        VLDR.F32 s1 , =0 ;data 2 (x2)
+        VLDR.F32 s2 , =0 ;data 3 (x3)	
+		B calculation
+		
+set_2	VLDR.F32 s0 , =1 ;data 1 (x1)
+        VLDR.F32 s1 , =0 ;data 2 (x2)
+        VLDR.F32 s2 , =1 ;data 3 (x3)
+		B calculation
+	
+	
+set_3	VLDR.F32 s0 , =1 ;data 1 (x1)
+        VLDR.F32 s1 , =1 ;data 2 (x2)
+        VLDR.F32 s2 , =0 ;data 3 (x3)
+		B calculation
+
+
+set_4	VLDR.F32 s0 , =1 ;data 1 (x1)
+        VLDR.F32 s1 , =1 ;data 2 (x2)
+        VLDR.F32 s2 , =1 ;data 3 (x3)
+		B calculation
 		
 		
-SR      MOV R0,0xA ; iteration value for expression expansion
-        MOV R1,#1; counting Variable 'i'
-        VLDR.F32 S0,=1;Holding the final value of sum of series elements 's' (e^x)
-        VLDR.F32 S1,=1;Temp Variable to hold the intermediate series elements 't'
+calculation       ;z=wo.x0 + w1.x1 + w2.x2
+				  VMUL.F32 s7,s0,s3   ;w0.x0
+				  VMUL.F32 s8,s1,s4   ;w1.x1
+				  VMUL.F32 s19,s2,s15   ;w2.x2
+				  VADD.F32 s18,s7,s8   ;w0.x0 + w1.x1
+				  VADD.F32 s20,s18,s19   ;z = w0.x0 + w1.x1 + w2.x2 ;Z is stored in s9
+				  VADD.F32 s22,s6,s20   ;z = w0.x0 + w1.x1 + w2.x2 + bias;Z is stored in s9
+   			     ;VADD.F32 s22,s22,s21   ;z = w0.x0 + w1.x1 + w2.x2 + bias;Z is stored in s9
+  				  B exp11
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+exp11           MOV R2,#10 ;   number of times the loop runs ,the value 'n'
+	            MOV R1,#1;initial value from which the loop starts 'i'
+				VLDR.F32 S10,=1;Holding the sum of series elements 's'
+				VLDR.F32 S11,=1;Temp Variable to hold the intermediate series elements 't'
+				;VLDR.F32 s22,=0   ;
+				B exp1
+
+
+exp1    		;VMOV.F32 S9,R9;Moving the bit stream in R1('i') to S5(floating point register)
+				CMP R1,R2;Compare 'i' and 'n' 
+				BLE exp;if i < n goto LOOP
+				B Sigmoid
+
+exp				VMUL.F32 S11,S11,S22;t = t*x
+				VMOV.F32 S5,R2;Moving the bit stream in R1('i') to S5(floating point register)
+				VCVT.F32.U32 S5, S5;Converting the bitstream into unsigned fp Number 32 bit
+ 				VDIV.F32 S11,S11,S5;Divide t by 'i' and store it back in 't'
+				VADD.F32 S11,S10,S11;Finally add 's' to 't' and store it in 's'
+				SUB R2,R2,#1;Increment the counter variable 'i'
+				B exp1;Again goto comparision	
+
+Sigmoid  		VLDR.F32 S12,=1
+				VDIV.F32 S11,S12,S11
+				VADD.F32 S13,S11,S12
+				VDIV.F32 S14,S12,S13
+				
+				;;;;;;;;;;;;;;;;;;;;;;;;;;;
+				MOV R0,R12	
+				VCVT.S32.F32 s0,s0
+				VCVT.S32.F32 s1,s1	
+				VCVT.S32.F32 s2,s2				
+				VMOV.F32 R1,s0
+				VMOV.F32 R2,s1
+				VMOV.F32 R3,s2
+				BL printMsg
+				;;;;;;;;;;;;;;;;;;;;;;
+				;VCVT.S32.F32 S14,S14				
+				VMOV.F32 R5,S14
+				VLDR.F32 S31,=0.5
+				
+				;VCVT.S32.F32 S31,S31
+				VMOV.F32 R6,S31;
+				CMP R5,R6
+				ITE LT
+				MOVLT R0,#0
+				MOVGE R0,#1
+				;MOV R0,R8
+				BL print1
+				;;;;;;;;;;;;;;;;;;;;;;;;;
+
+				
+				MOV R5,#6 ;
+				MOV R6,#5 ;
+				MOV R7,#4 ;				
+				MOV R8,#3;
+				MOV R9,#2 ;
+				MOV R10,#1 ; 
+				
+				
+				SUB R11,R11,#1
+				
+				CMP R11,R5
+				BEQ set_2 
 		
-LOOP1   CMP R1,R0;Compare 'i' and 'n'
-        BLE LOOP;if i < n goto LOOP
-		BX lr
+				CMP R11,R6
+				BEQ set_3 
+		
+				CMP R11,R7
+				BEQ set_4 
+				
+				B loop_logic
+				
+loop_logic		SUB R12,R12,#1
+				
+				MOV R11,#7 ; 				
+				CMP R12,R5
+				BEQ logic_or
+				
+				CMP R12,R6
+				BEQ logic_not
+		
+				CMP R12,R7
+				BEQ logic_nand
+		
+				CMP R12,R8
+				BEQ logic_nor
+		
+				CMP R12,R9
+				BEQ logic_xor
+		
+				CMP R12,R10
+				BEQ logic_xnor	
+				
+stop       B   stop		
 
-LOOP    VMUL.F32 S1,S1,S2; t = t*x
-        B fact ;calculte the factorial value of counting variable stored in R1(i) which further will used in denominator of expression
-facm    VMOV.F32 S5,R7;Moving the value of i! in R7 = R1!('i!') to S5(floating point register)
-        VCVT.F32.U32 S5,S5;Converting the value present in R7(i!) into unsigned fp Number 32 bit
-        VDIV.F32 S3,S1,S5;Divide t by 'i!' and store it into new temperory variable s3
-        VADD.F32 S0,S0,S3;Finally add 's' to 'S3' and store it in 's'
-        ADD R1,R1,#1;Increment the counter variable 'i'
-        B LOOP1;Again goto comparision
-
- ; factorial program to find the factorial of counting variable 'i' 
-fact	MOV r7,#1 ; if n = 0, at least 'i!' = 1
-        MOV R6,R1 ; move value of R1('i') to R6 ('n') to store intermediate values during operation
-loop2   CMP r6, #0 ; compare the value of n to zero
-        MULGT r7, r6, r7 ; if i!=0
-        SUBGT r6, r6, #1 ; decrement 'n'
-        BGT loop2 ; do another mul if counter!= 0
-        B facm ; pass the final factorial value when n=0
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         endfunc
-        end
+      end				  
+				  
